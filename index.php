4378e022160d69209b3b2a61aae635ffa75af9ba
ThatcherReidel <124378590+ThatcherReidel@users.noreply.github.com>
2023-02-27 15:30:19 +0000

    work

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
   </head>
 
   <body>
+ 
+  <form action="handler.php" method="post">
+Name: <input type="text" name="name"><br>
+<input type="submit">
+</form>
 
     <?php
     echo "<h1>Hello, World!</h1>\n";
