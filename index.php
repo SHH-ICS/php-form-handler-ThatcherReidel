e8c08ae836092cf1e557de98bd26a4f1d5c948e3
ThatcherReidel <124378590+ThatcherReidel@users.noreply.github.com>
2023-02-27 15:28:24 +0000

    ok

--- index.php
+++ index.php
@@ -5,13 +5,11 @@
   </head>
 
   <body>
-    
+
     <?php
     echo "<h1>Hello, World!</h1>\n";
     ?>
-    
-    Welcome <?php echo $_GET["name"]; ?><br>
-Your email address is: <?php echo $_GET["email"]; ?>
+
   </body>
-  
+
 </html>
